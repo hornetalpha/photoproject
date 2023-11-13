9d75f5327eebe0a7ac85e8b3a54afc7f9e0642ff
djangouser <utm2377031@stu.o-hara.ac.jp>
2023-11-13 09:39:23 +0900

    Commit

---
+++ accounts/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+# CustomUserクラスをインポート
+from .models import CustomUser
+
+class CustomUserAdmin(admin.ModelAdmin):
+    # レコード一覧にidとusernameを表示
+    list_display = ('id', 'username')
+    # 表示するカラムにリンクを設定
+    list_display_links = ('id', 'username')
+
+# Django管理サイトにCustomUser、CustomUserAdminを登録する
+admin.site.register(CustomUser, CustomUserAdmin)
